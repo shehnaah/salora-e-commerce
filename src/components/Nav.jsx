4f2ba4f00504df2467d667a6bcfd6f5bd73a6150
shehnaah <shehna0501@gmail.com>
2024-08-22 15:29:43 +0530

    third

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,15 +20,15 @@ function Nav() {
   }, []);
   return (
 <div>
-<nav class="navbar bg-transparent fixed-top">
+<nav class="navbar navbar-expand-lg bg-transparent fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Salora</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div class="offcanvas offcanvas-end d-lg-none" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style={{paddingLeft:'50px',fontSize:'1.8rem'}}>Salora</h5>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel" style={{paddingLeft:'50px',fontSize:'1.8rem'}} >Salora</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -53,6 +53,29 @@ function Nav() {
           <i class="fa-solid fa-phone-flip fa-sm"></i> (205) 444-0100
         </button>
       </div>
+    </div>
+
+    <div class="collapse navbar-collapse d-none d-lg-flex justify-content-between" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0 text-black">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
+      <button class="btn btn-light navbar-button disabled text-dark">
+        <i class="fa-solid fa-phone-flip fa-sm"></i> (205) 444-0100
+      </button>
     </div>
   </div>
 </nav>
